703eb6fba1dffbc3c7a2d21d14fcfe00f570b52a
MauricioManjarres <mauricio.manjarresd@pca.edu.co>
2026-02-26 07:35:11 -0500

    arreglando redireccionamiento a gmail desde el formulario de contacto

--- components/Contacto.tsx
+++ components/Contacto.tsx
@@ -35,7 +35,8 @@ export default function Contacto() {
             <input type="hidden" name="_captcha" value="false" />
             <input type="hidden" name="_subject" value="Nuevo Lead Lidanova 🚀" />
             <input type="hidden" name="_template" value="table" />
-            <input type="hidden" name="_next" value="https://www.lidanova.com/gracias" />
+            <input type="hidden" name="_next" value="/gracias" />
+            <input type="hidden" name="_autoresponse" value="Gracias por escribirnos 🚀" />
           </form>
 
         </div>
